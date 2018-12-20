51c4e2d419389e0d5e9688872997bf7155c526d8
Isaac Lozano <Isaac-Lozano@users.noreply.github.com>
2018-12-19 22:56:43 -0800

    main: conditional compilation for cross-platform

--- src/main.rs
+++ src/main.rs
@@ -17,31 +17,23 @@ use crate::controller_state::ControllerState;
 use crate::input_reader::InputReader;
 use crate::input_reader::dtm_reader::DtmReader;
 use crate::input_reader::serial_reader::SerialReader;
+#[cfg(windows)]
 use crate::input_reader::sa2_reader::Sa2Reader;
 
 const SONIC_ADVENTURE_2_EXE: &'static str = "sonic2App.exe";
 
 fn main() {
+    // Print out version info.
+    println!("GC Input Viewer by OnVar.");
+    println!("Version {}", env!("CARGO_PKG_VERSION"));
+
     // Read from configuration file.
     let conf = Configuration::from_path("conf.yaml").unwrap_or_barf("Error opening \"conf.yaml\" configuration file");
     let theme = conf.theme;
     let base = conf.theme_path;
+
     // Take input from whatever input method is specified in the config file.
-    let mut reader: Box<dyn InputReader> = match conf.input {
-        InputSource::Dtm(path) => {
-            let dtm_reader = DtmReader::from_path(&path).unwrap_or_barf("Could not open dtm file");
-            Box::new(dtm_reader)
-        }
-        InputSource::Sa2(exe_name_opt) => {
-            let exe_name = exe_name_opt.unwrap_or(SONIC_ADVENTURE_2_EXE.into());
-            let sa2_reader = Sa2Reader::new(exe_name).unwrap_or_barf("Could not open SA2 reader");
-            Box::new(sa2_reader)
-        }
-        InputSource::Serial(path) => {
-            let serial_reader = SerialReader::from_path(&path).unwrap_or_barf("Could not open serial port");
-            Box::new(serial_reader)
-        }
-    };
+    let mut reader = get_input(conf.input);
 
     // Make a controller state to share across threads.
     let state_mutex = Arc::new(Mutex::new(ControllerState::default()));
@@ -71,6 +63,42 @@ fn main() {
         // Update mutex.
         let mut state = state_mutex.lock().unwrap_or_barf("Error updating controller state");
         *state = new_state;
+    }
+}
+
+#[cfg(windows)]
+fn get_input(input: InputSource) -> Box<dyn InputReader> {
+    match input {
+        InputSource::Dtm(path) => {
+            let dtm_reader = DtmReader::from_path(&path).unwrap_or_barf("Could not open dtm file");
+            Box::new(dtm_reader)
+        }
+        InputSource::Sa2(exe_name_opt) => {
+            let exe_name = exe_name_opt.unwrap_or(SONIC_ADVENTURE_2_EXE.into());
+            let sa2_reader = Sa2Reader::new(exe_name).unwrap_or_barf("Could not open SA2 reader");
+            Box::new(sa2_reader)
+        }
+        InputSource::Serial(path) => {
+            let serial_reader = SerialReader::from_path(&path).unwrap_or_barf("Could not open serial port");
+            Box::new(serial_reader)
+        }
+    }
+}
+
+#[cfg(not(windows))]
+fn get_input(input: InputSource) -> Box<dyn InputReader> {
+    match input {
+        InputSource::Dtm(path) => {
+            let dtm_reader = DtmReader::from_path(&path).unwrap_or_barf("Could not open dtm file");
+            Box::new(dtm_reader)
+        }
+        InputSource::Sa2(exe_name_opt) => {
+            barf("Process memory reading only available on Windows")
+        }
+        InputSource::Serial(path) => {
+            let serial_reader = SerialReader::from_path(&path).unwrap_or_barf("Could not open serial port");
+            Box::new(serial_reader)
+        }
     }
 }
 
